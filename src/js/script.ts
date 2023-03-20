f705c3b5fd7994d52406d2a3723ce7630bc9a04c
Bikrant Jung <bikrantjung1214@gmail.com>
2023-03-20 17:19:06 +0545

    Creating Line

--- src/js/script.ts
+++ src/js/script.ts
@@ -1,34 +1,49 @@
 import * as three from "three";
 const canvasWrapper = document.getElementById("canvas-wrapper");
-// Creating a scene
+/*
+  Creating a scene, camera, and renderer and appending to the DOM.
+*/
 const scene = new three.Scene();
 
-// Creating a camera
+// Creating camera, position and lookAt
 const camera = new three.PerspectiveCamera(
-  75,
+  45,
   window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+  1,
+  500
 );
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
+// Creating a renderer
 const renderer = new three.WebGL1Renderer();
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 canvasWrapper.appendChild(renderer.domElement);
 
-const geometry = new three.BoxGeometry(1, 1, 1);
-const material = new three.MeshBasicMaterial({
-  color: "rgb(234,222,135)",
-});
+/*
+  Creating a material, think this material as iron, we can shape any objects from iron.
+  Here we are creating a line from a material
+*/
+const material = new three.LineBasicMaterial({ color: "blue" });
 
-const cube = new three.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
+// Creating X,Y,Z points for a line
+const points = [];
+points.push(new three.Vector3(-10, 0, 0));
+points.push(new three.Vector3(0, 10, 0));
+points.push(new three.Vector3(10, 0, 0));
 
+// Setting the geometry
+const geometryTriangle = new three.BufferGeometry().setFromPoints(points);
+
+// Creating a Line
+const triangle = new three.Line(geometryTriangle, material);
+
+scene.add(triangle);
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // line.rotation.x += 0.01;
+  // line.rotation.y += 0.01;
+  // line.rotation.z += 0.01;
   renderer.render(scene, camera);
 }
 animate();
